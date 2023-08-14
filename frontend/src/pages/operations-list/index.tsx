982f00c77ec24e1247f056f4b621f3f59d41e7af
Peter Konokhov <peter.konokhov@tempostorm.com>
2023-08-14 23:58:20 +0300

    webhook test

--- frontend/src/pages/operations-list/index.tsx
+++ frontend/src/pages/operations-list/index.tsx
@@ -3,7 +3,7 @@ import { useGetOperationsQuery } from 'shared/api/operationAPI'
 const OperationsListPage = () => {
   const { data } = useGetOperationsQuery('1')
   console.log(data)
-  return <div>OperationsPage TEST</div>
+  return <div>OperationsPage TEST 2</div>
 }
 
 export default OperationsListPage
