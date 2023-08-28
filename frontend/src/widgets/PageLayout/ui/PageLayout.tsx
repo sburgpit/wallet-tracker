e2df60216d20e720c7dca856db0c79f8e3163004
Peter Konokhov <peter.konokhov@tempostorm.com>
2023-08-28 19:58:14 +0300

    debug telegram viewport

--- frontend/src/widgets/PageLayout/ui/PageLayout.tsx
+++ frontend/src/widgets/PageLayout/ui/PageLayout.tsx
@@ -1,8 +1,14 @@
 import { Outlet } from 'react-router-dom'
+import { PageLoader } from 'widgets/PageLoader'
 
 export const PageLayout = () => {
   return (
     <main>
+      <span>vh: {getComputedStyle(document.querySelector('html')!).getPropertyValue('--tg-viewport-height')}</span>
+      <br />
+      <span>
+        vsh: {getComputedStyle(document.querySelector('html')!).getPropertyValue('--tg-viewport-stable-height')}
+      </span>
       <Outlet />
     </main>
   )
