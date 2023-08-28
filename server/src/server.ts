c9c5ce3b50c1e7c23400d5441e90b53cc1dd2eda
Peter Konokhov <peter.konokhov@tempostorm.com>
2023-08-28 19:33:34 +0300

    disable telegram service for development

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,6 @@ import express from 'express'
 import payload from 'payload'
 import TelegramService from './services/telegram.service'
 import CurrencyService from './services/currency.service'
-import CMCService from './services/cmc.service'
 
 require('dotenv').config()
 const app = express()
@@ -18,8 +17,10 @@ const start = async () => {
     express: app,
     onInit: async () => {
       payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
-      new TelegramService()
-      new CurrencyService()
+      if (process.env.NODE_ENV === 'production') {
+        new TelegramService()
+        new CurrencyService()
+      }
     },
   })
 
